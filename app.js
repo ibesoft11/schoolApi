d4203014927fcc331fca42a43da34a102d6da826
Ibesoft11 <ibesoft11@gmail.com>
2018-06-08 12:23:24 +0100

    implemented routing functions for viewing student records, adding and deleting a student

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var app = express();
+var School = require('./schoolSystem');
+var school = School("Renaissance University");
 
 app.listen(3000, function (){
     console.log("Server started, running on port 3000");
 });
 
 app.get('/', function(req, res){
-    
+   res.send(school.getStudents());
 });
+
+app.get('/api/students/add', function(req, res){
+    let student = school.student(req.query.name, req.query.department, req.query.level);
+    school.addStudent(student);
+    school.getStudents();
+});
+
+app.get('/api/students/delete', function(req, res){
+    school.deleteStudent(req.query.id);
+    school.getStudents();
+});
+
+app.get('/api/students/update', function(req, res)){
+    
+}
