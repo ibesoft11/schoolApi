ad70ddaef8700ace0e5511cdf9014a820841c595
Ibesoft11 <ibesoft11@gmail.com>
2018-06-08 11:25:32 +0100

    Added functions to read/write data from/to our json database

--- schoolSystem.js
+++ schoolSystem.js
@@ -4,14 +4,20 @@ function School(name){
     this.name = name;
     this.students = [];
 }
-
+School.prototype.getData = function(){
+    return JSON.parse(fs.readFileSync('./studentDb.json', 'utf-8'));
+}   
+School.prototype.writeData = function(students){
+    return fs.writeFileSync('./studentDb.json', JSON.stringify(students));
+}
 School.prototype.student = function(name, department, level){
     //generate stud id here.
     this.name = name;
     this.department = department;
     this.level = level;
 }
-School.prototype.addStudent = function(name, department, level){
+School.prototype.addStudent = function(student){
+
     //add student here
 }
 School.prototype.deleteStudent = function(id){
@@ -22,9 +28,6 @@ School.prototype.updateStudent = function(id, param, value){
 }
 School.prototype.getStudentById = function(id){
     //print student's data here
-}
-School.prototype.getStudents = function(){
-    //return every student
 }
 School.prototype.getStudents = function(param, value){
     if (arguments.length == 0){
